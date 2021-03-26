5425b081abec9ea972abd4227dce06f346b99701
Anders Kielsholm <anders@anigif.dk>
2021-03-26 01:56:53 +0100

    Add a lot of checks to the input field so it hopefully doesn't throw any errors and are showing correct state at all times (without overriding when not needed)

--- src/jsMain/kotlin/ReactLocalDateInput.kt
+++ src/jsMain/kotlin/ReactLocalDateInput.kt
@@ -1,8 +1,14 @@
+import kotlinx.css.Color
+import kotlinx.css.Display
+import kotlinx.css.color
+import kotlinx.css.display
 import kotlinx.datetime.LocalDate
 import kotlinx.html.js.onChangeFunction
 import org.w3c.dom.HTMLInputElement
 import react.*
 import react.dom.input
+import styled.css
+import styled.styledP
 
 external interface ReactLocalDateInputProps : RProps {
     var selectedDate: LocalDate
@@ -11,27 +17,80 @@ external interface ReactLocalDateInputProps : RProps {
 
 external interface ReactLocalDateInputState : RState {
     var error: String?
+    var lastPropDate: LocalDate?
+    var dateString: String
 }
 
 @JsExport
 class ReactLocalDateInput : RComponent<ReactLocalDateInputProps, ReactLocalDateInputState>() {
 
+    companion object : RStatics<ReactLocalDateInputProps, ReactLocalDateInputState, ReactLocalDateInput, Nothing>(
+        ReactLocalDateInput::class
+    ) {
+        init {
+            getDerivedStateFromProps = { props, state ->
+                if (state.lastPropDate != props.selectedDate) {
+                    state.lastPropDate = props.selectedDate
+
+                    val currentDate = try {
+                        state.dateString.toDate()
+                    } catch (exception: IllegalArgumentException) {
+                        null
+                    }
+
+                    // Checking this ensures we won't change the input text if the input is valid but not as we would
+                    // insert. Can happen if the user types: 01-01-2021 => 01-0-2021 => 01-02-2021
+                    if (currentDate != props.selectedDate) {
+                        state.dateString = props.selectedDate.let { date ->
+                            "${date.dayOfMonth}-${date.monthNumber}-${date.year}"
+                        }
+                    }
+
+                    state.error = null
+                }
+
+                state
+            }
+        }
+    }
+
+    override fun ReactLocalDateInputState.init() {
+        dateString = ""
+    }
+
     override fun RBuilder.render() {
         input {
-            attrs.value =
-                "${props.selectedDate.dayOfMonth}/${props.selectedDate.monthNumber}/${props.selectedDate.year}"
+            attrs.value = state.dateString
 
             attrs.onChangeFunction = {
                 val target = it.target as HTMLInputElement
-                val dateParts = target.value.split("/")
-                props.onDateSelected(
-                    LocalDate(
-                        dayOfMonth = dateParts[0].toInt(),
-                        monthNumber = dateParts[1].toInt(),
-                        year = dateParts[2].toInt()
-                    )
-                )
+                setState {
+                    dateString = target.value
+                    error = try {
+                        val date = target.value.toDate()
+
+                        // Note: It seems like props update as a reaction to calling this will be executed after
+                        // updating the local state
+                        props.onDateSelected(date)
+
+                        null
+                    } catch (exception: IllegalArgumentException) {
+                        exception.message
+                    }
+                }
             }
+        }
+        styledP {
+            css {
+                color = Color.darkRed
+                display = if (state.error == null) {
+                    Display.none
+                } else {
+                    Display.block
+                }
+            }
+
+            +(state.error ?: "")
         }
     }
 }
@@ -40,3 +99,39 @@ fun RBuilder.localDateInput(handler: ReactLocalDateInputProps.() -> Unit) =
     child(ReactLocalDateInput::class) {
         this.attrs(handler)
     }
+
+private fun String.toDate(): LocalDate {
+    val dateParts = split("-").map { datePart ->
+        datePart.trim()
+    }
+
+    if (dateParts.size < 3 || dateParts.any { it.isEmpty() }) {
+        throw IllegalArgumentException("Forventet format: DD-MM-YYYY")
+    }
+
+    val dayOfMonth = dateParts[0].toIntOrNull()
+    val monthNumber = dateParts[1].toIntOrNull()
+    val year = dateParts[2].toIntOrNull()
+
+    if (dayOfMonth == null || monthNumber == null || year == null) {
+        throw IllegalArgumentException("Forventet format: DD-MM-YYYY. Nogle af elementerne var ikke tal")
+    } else if (dayOfMonth < 1 || dayOfMonth > 31) {
+        throw IllegalArgumentException("Dagen skal være mellem 1-31")
+    } else if (monthNumber < 1 || monthNumber > 12) {
+        throw IllegalArgumentException("Måneden skal være mellem 1-12")
+    } else if (year < 1583) {
+        throw IllegalArgumentException("År mindre end 1583 er desværre ikke supporteret")
+    }
+
+    try {
+        return LocalDate(
+            dayOfMonth = dayOfMonth,
+            monthNumber = monthNumber,
+            year = year
+        )
+    } catch (exception: IllegalArgumentException) {
+        throw IllegalArgumentException(
+            "Datoen er ugyldig - muligvis fordi den ikke eksisterer. Forventet format: DD-MM-YYYY"
+        )
+    }
+}
