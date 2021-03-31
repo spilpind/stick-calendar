2d3e5963a771c4a9839f5dcc58df723fa82e4196
Anders Kielsholm <anders@anigif.dk>
2021-04-01 01:21:52 +0200

    Make it possible to output current date and days left of the year

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,9 +1,24 @@
+import it.woar.stickcalendar.StickCalendar.lengthOfYear
+import it.woar.stickcalendar.StickCalendar.toStickDate
 import kotlinx.browser.document
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.todayAt
 import react.dom.render
 
 fun main() {
     render(document.getElementById("stick-calendar-root")) {
         child(ReactGregToStickApp::class) { }
         child(ReactStickToGregApp::class) { }
+
+        stickCalendarComponentsByClassName("stick-calendar-today") {
+            val today = Clock.System.todayAt(TimeZone.currentSystemDefault()).toStickDate()
+            +today.toExtendedFullString()
+        }
+
+        stickCalendarComponentsByClassName("stick-calendar-days-left-of-year") {
+            val today = Clock.System.todayAt(TimeZone.currentSystemDefault()).toStickDate()
+            +"${lengthOfYear(today.year) - today.day}"
+        }
     }
 }
