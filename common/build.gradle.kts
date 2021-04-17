1911d439087d7bec1bbe1373e761a79453cdb32f
Anders Kielsholm <anders@anigif.dk>
2021-04-17 23:34:46 +0200

    Remove/suppress warnings and add extra line breaks. Regarding suppressing: https://youtrack.jetbrains.com/issue/KT-38871

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,6 +11,7 @@ kotlin {
             useJUnit()
         }
     }
+
     js(LEGACY) {
         browser {
             commonWebpackConfig {
@@ -25,40 +26,45 @@ kotlin {
         }
         binaries.executable()
     }
+
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
+    when {
         hostOs == "Mac OS X" -> macosX64("native")
         hostOs == "Linux" -> linuxX64("native")
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
-
     sourceSets {
+
+        @Suppress("UNUSED_VARIABLE")
         val commonMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.1")
             }
         }
+
+        @Suppress("UNUSED_VARIABLE")
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
             }
         }
+
+        @Suppress("UNUSED_VARIABLE")
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
             }
         }
-        val jsMain by getting
+
+        @Suppress("UNUSED_VARIABLE")
         val jsTest by getting {
             dependencies {
                 implementation(kotlin("test-js"))
             }
         }
-        val nativeMain by getting
-        val nativeTest by getting
     }
 }
